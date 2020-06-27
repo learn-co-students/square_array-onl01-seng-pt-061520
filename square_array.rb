191fc8b5577d525fdf5180022ce04a2816815b6e
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-27 19:18:06 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+# def square_array(array)
+#   newArr= []
+#   array.each do |number|
+#   puts "I like #{number}."
+#   return newArr << number **2
+# end
+# end
+
 def square_array(array)
-  # your code here
+	  arr = []
+  array.each { |number| arr << number ** 2}
+  return arr
 end
